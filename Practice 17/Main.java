ce4b19d7997cc5fdcb87ff0132f1be6354743d4e
MajorFedor <95159668+MajorFedor@users.noreply.github.com>
2025-06-16 00:29:42 +0300

    Delete Practice 17 directory

--- Practice 17/Main.java
+++
@@ -1,9 +0,0 @@
-import Settings.*;
-import Game.*;
-
-public class Main {
-    public static void main(String[] args){
-        LoadSettings.checkExistingSettings();
-        Menu.showMenu();
-    }
-}
--- Practice 17/Settings/SettingsData.java
+++
@@ -1,8 +0,0 @@
-package Settings;
-
-public class SettingsData {
-    public static int boardSize = 3;
-    public static String firstPlayer = "PlayerX";
-    public static String secondPlayer = "PlayerO";
-    public static String settings = "settings.txt";
-}
